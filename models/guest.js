e0453c9b51762995a479228baf93087d2e85c875
Sean Haughey <sdhaughey@gmail.com>
2015-10-20 09:22:38 -0500

    changed schemas and placement of nav bar

--- models/guest.js
+++ models/guest.js
@@ -2,19 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var guestSchema = new Schema({
-	first_name: { type: String, required: true },
-	last_name: { type: String, required: true },
+	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	reviews: { type: Array, required: true },
 	rating: { type: Number, required: true },
-	address: {
-		number: Number,
-		street: String,
-		city: String,
-		state: String,
-		zip_code: Number
-		},
-	phone: Number,
 	rev_count: Number
 })
 
--- models/user.js
+++ models/user.js
@@ -3,20 +3,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	first_name: { type: String, required: true },
-	last_name: { type: String, required: true },
+	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	password_hash: { type: String, required: true },
-	address: {
-		number: Number,
-		street: String,
-		city: String,
-		state: String,
-		zip_code: Number
-		},
-	phone: Number,
-	position: String,
-	employer: String,
 	rev_count: Number
 })
 
